ccbd775c1f3be1b18fab89926b7e705b6e3f3ea8
angeltamuc <acampos21@leomail.tamuc.edu>
2025-05-01 21:53:35 -0500

    Updated Levels 1,2,& 3 to guess on individual cards, and to flip the card if the guess was right.

--- src/main/java/com/ood/groupOne/card_guessing_game/Level3.java
+++ src/main/java/com/ood/groupOne/card_guessing_game/Level3.java
@@ -18,8 +18,7 @@ public class Level3 {
     private Card[] selectedCards = new Card[3];
     private Image cardBackImage;
     private Label timerLabel;
-    private Label retryLabel = null;
-    private Button retryLevelButton = null;
+    private boolean[] correctGuess = new boolean[3];
 
     public Level3(MainApp mainApp) {
         this.mainApp = mainApp;
@@ -38,15 +37,14 @@ public class Level3 {
         HBox cardRow = new HBox(20);
         cardRow.setAlignment(Pos.CENTER);
 
-//        Random rand = new Random();
-//        Card[][] cards = mainApp.getCards();
 
         for (int i = 0; i < 3; i++) {
-//            int suitIndex = rand.nextInt(4);
-//            int rankIndex = rand.nextInt(13);
 
             Card drawnCard = mainApp.getDeck().drawCard();
             selectedCards[i] = drawnCard;
+
+            VBox cardBox = new VBox(10);
+            cardBox.setAlignment(Pos.CENTER);
 
             ImageView cardBack = new ImageView(cardBackImage);
             cardBack.setFitWidth(100);
@@ -61,107 +59,46 @@ public class Level3 {
             suitField.setPromptText("Suit (lowercase only)");
             suitFields[i] = suitField;
 
-            VBox cardBox = new VBox(10);
-            cardBox.setAlignment(Pos.CENTER);
-            cardBox.getChildren().addAll(cardBack, rankField, suitField);
+            Button guessButton = new Button("Submit Guess");
+            int index = i;
+            guessButton.setOnAction(e -> handleGuess(index));
 
+            cardBox.getChildren().addAll(cardBack, rankField, suitField, guessButton);
             cardRow.getChildren().add(cardBox);
         }
 
-        Button submitButton = new Button("Submit Guess");
-        submitButton.setOnAction(e -> {
-            checkGuesses();
-        });
 
-        layout.getChildren().addAll(timerLabel, levelLabel, cardRow, submitButton);
+        layout.getChildren().addAll(timerLabel, levelLabel, cardRow);
     }
-    // Check
-    private void checkGuesses() {
-        int correct = 0;
+    private void handleGuess(int index) {
+        if (correctGuess[index]) return;
 
-        for (int i = 0; i < 3; i++) {
-            String rankGuess = rankFields[i].getText();
-            String suitGuess = suitFields[i].getText();
+        String rankGuess = rankFields[index].getText();
+        String suitGuess = suitFields[index].getText();
+        String actualRank = selectedCards[index].rank();
+        String actualSuit = selectedCards[index].suit();
 
-            String actualRank = selectedCards[i].rank();
-            String actualSuit = selectedCards[i].suit();
-
-            if (rankGuess.equals(actualRank) && suitGuess.equals(actualSuit)) {
-                correct++;
-            }
+        if (rankGuess.equals(actualRank) && suitGuess.equals(actualSuit)) {
+            correctGuess[index] = true;
+            flipCard(index);
         }
-        flipCardsFaceUp();
-        if (correct == 3) {
-            flipCardsFaceUp();
+
+        if (allCorrect()) {
             mainApp.startLevel(4);
-        } else {
-            showRetryOption("Wrong! Try again.");
-        }
-    }
-    //flip card
-    private void flipCardsFaceUp() {
-        ArrayList<ImageView> cardImages = mainApp.getCardImages();
-
-        for (int i = 0; i < 3; i++) {
-            Card card = selectedCards[i];
-
-//            int suitIndex = getSuitIndex(card.suit());
-//            int rankIndex = getRankIndex(card.rank());
-//
-//            if (suitIndex == -1 || rankIndex == -1) {
-//                System.out.println("Invalid suit for card " + i + ": " + card.suit() + " / " + card.rank());
-//                selectedCardViews[i].setImage(cardBackImage); // Keep showing back image
-//            } else {
-//                Image faceImage = cardImages[suitIndex][rankIndex].getImage();
-//                selectedCardViews[i].setImage(faceImage);
-//            }
-            Image faceImage;
-            faceImage = cardImages.get(mainApp.getDeck().deck().indexOf(card)).getImage();
-            selectedCardViews[i].setImage(faceImage);
         }
     }
 
-    private void showRetryOption(String messageText) {
-        //remove old labels/buttons
-        if (retryLabel != null) layout.getChildren().remove(retryLabel);
-        if (retryLevelButton != null) layout.getChildren().remove(retryLevelButton);
-
-        retryLabel = new Label(messageText);
-        retryLevelButton = new Button("Retry Level 2");
-        retryLevelButton.setOnAction(e -> mainApp.startLevel(2));
-
-        layout.getChildren().addAll(retryLabel, retryLevelButton);
+    private void flipCard(int index) {
+        Card card = selectedCards[index];
+        Image faceImage = mainApp.getCardImages().get(mainApp.getDeck().deck().indexOf(card)).getImage();
+        selectedCardViews[index].setImage(faceImage);
     }
 
-
-
-    private int getSuitIndex(String suit) {
-        switch (suit) {
-            case "clubs": return 0;
-            case "diamonds": return 1;
-            case "hearts": return 2;
-            case "spades": return 3;
-            default: return -1;
+    private boolean allCorrect() {
+        for (boolean correct : correctGuess) {
+            if (!correct) return false;
         }
-    }
-
-    private int getRankIndex(String rank) {
-        switch (rank) {
-            case "ace": return 0;
-            case "2": return 1;
-            case "3": return 2;
-            case "4": return 3;
-            case "5": return 4;
-            case "6": return 5;
-            case "7": return 6;
-            case "8": return 7;
-            case "9": return 8;
-            case "10": return 9;
-            case "jack": return 10;
-            case "queen": return 11;
-            case "king": return 12;
-            default: return -1;
-        }
+        return true;
     }
 
     public VBox getLayout() {
